22281d669ec42499a27ea851f1508d8a52b5d934
Ibán López <iban@solvefy.cloud>
2024-02-12 14:38:31 -0300

    refactor: updates pool strategy on node server

--- docker_nginx/app.js
+++ docker_nginx/app.js
@@ -4,35 +4,41 @@ const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
-const db = mysql.createConnection({
-  host: 'mysql',
+const db = mysql.createPool({
+  connectionLimit: 10, 
+  host: 'mysql', 
   user: 'root',
   password: 'password',
   database: 'fullcycle'
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error('Error connecting to MySQL database:', err);
     return;
   }
-  console.log('Conexão com o banco de dados MySQL estabelecida!');
+  console.log('Connected to MySQL database!');
+  connection.release(); 
 });
 
 app.get('/', (req, res) => {
-  const name = 'John Doe'; // Nome a ser cadastrado
-  const sql = `INSERT INTO people (name) VALUES ('${name}')`;
-  db.query(sql, (err, result) => {
+  const name = 'John Doe'; 
+  const ipaddr = req.ip; 
+
+  const sql = `INSERT INTO people (name, ipaddr) VALUES (?, ?)`;
+  const values = [name, ipaddr];
+  
+  db.query(sql, values, (err, result) => {
     if (err) {
-      console.error('Erro ao inserir nome no banco de dados:', err);
-      res.status(500).send('Erro interno do servidor');
+      console.error('Error inserting data into database:', err);
+      res.status(500).send('Internal Server Error');
       return;
     }
-    console.log('Nome cadastrado no banco de dados');
-    res.send('<h1>Full Cycle Rocks!</h1>');
+    console.log('Data inserted into database');
+    res.send(`<h1>Full Cycle Rocks!</h1><h2>Your IP address: ${ipaddr}</h2>`);
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor Node.js rodando na porta ${PORT}`);
+  console.log(`Node.js server running on port ${PORT}`);
 });
